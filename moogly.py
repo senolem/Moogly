80b8da2bb7f40683cfe982a307349688f8c6f7f3
senolem <37041587+senolem@users.noreply.github.com>
2024-05-05 18:19:32 +0200

    v2

--- moogly.py
+++ moogly.py
@@ -1,6 +1,7 @@
 import discord
 from discord.ext import commands
 import json
+import sqlite3
 import traceback
 
 class BotClient(commands.Bot):
@@ -15,17 +16,31 @@ class BotClient(commands.Bot):
             help_command=None,
         )
 
-        # Data
-        self.application_data = {}
+        # Connect to SQLite database
+        self.db_conn = sqlite3.connect(config.database)
+        self.db_cursor = self.db_conn.cursor()
+
+        # Initialize database tables if not exist
+        self.create_tables()
+
         self.config = config
 
     async def on_ready(self):
         print(f'Logged in as {self.user} (ID: {self.user.id})')
         print('------')
 
-    async def setup_hook(self) -> None:
-        await self.tree.sync(guild=self.get_guild(self.config["guild_id"]))
+    def create_tables(self):
+        # Create the application_data table if it doesn't exist
+        self.db_cursor.execute("""
+        CREATE TABLE IF NOT EXISTS application_data (
+            user_id INTEGER PRIMARY KEY,
+            fc TEXT,
+            ingame_name TEXT
+        )
+        """)
+        self.db_conn.commit()
 
+# UI Name modal
 class ApplicationModal(discord.ui.Modal, title='Access application'):
     name = discord.ui.TextInput(
         label='What is your in-game name?',
@@ -33,20 +48,30 @@ class ApplicationModal(discord.ui.Modal, title='Access application'):
     )
 
     async def on_submit(self, interaction: discord.Interaction):
-        application_data = bot.application_data.get(interaction.user.id)
-        if application_data:
-            application_data['ingame_name'] = self.name.value
-            application_channel = bot.get_channel(bot.config["admission_channel_id"])
-            message_content = f'New application from {interaction.user.mention} (ID: {interaction.user.id}):\nIn-game name: {self.name.value}\nFC: {application_data["fc"]}'
-            await application_channel.send(message_content, view=AdmissionMessage())
-            await interaction.response.send_message(f'Application sent, awaiting approval...', ephemeral=True)
-        else:
-            await interaction.response.send_message('Oops! Something went wrong.', ephemeral=True)
+        if not self.name.value or len(self.name.value.split(' ')) != 2:
+            await interaction.response.send_message('Error: Invalid in-game name (format: Name LastName)', ephemeral=True)
+            return
 
-    async def on_error(self, interaction: discord.Interaction, error: Exception) -> None:
-        await interaction.response.send_message('Oops! Something went wrong.', ephemeral=True)
+        bot.db_cursor.execute("SELECT * FROM application_data WHERE user_id=?", (interaction.user.id,))
+        existing_data = bot.db_cursor.fetchone()
+        if existing_data:
+            bot.db_cursor.execute("UPDATE application_data SET ingame_name=? WHERE user_id=?", (self.name.value, interaction.user.id))
+        else:
+            bot.db_cursor.execute("INSERT INTO application_data (user_id, ingame_name) VALUES (?, ?)", (interaction.user.id, self.name.value))
+        bot.db_conn.commit()
+
+        application_data = {"fc": bot.application_data[interaction.user.id], "ingame_name": self.name.value}
+
+        application_channel = bot.get_channel(bot.config["admission_channel_id"])
+        message_content = f'New application from {interaction.user.mention} (ID: {interaction.user.id}):\nIn-game name: {self.name.value}\nFC: {application_data["fc"]}'
+        await application_channel.send(message_content, view=AdmissionMessage())
+        await interaction.response.send_message(f'Application sent, awaiting approval...', ephemeral=True)
+
+    async def on_error(self, interaction: discord.Interaction, error: Exception):
+        await interaction.response.send_message('Oops! Something went wrong. Please try again', ephemeral=True)
         traceback.print_exception(type(error), error, error.__traceback__) # Make sure we know what the error actually is
 
+# Approve/Deny view
 class AdmissionMessage(discord.ui.View):
     def extract_user_id(self, message_content: str) -> int:
         start_index = message_content.find("(ID: ") + len("(ID: ")
@@ -57,52 +82,76 @@ class AdmissionMessage(discord.ui.View):
     @discord.ui.button(label='Approve', style=discord.ButtonStyle.green)
     async def approve_button(self, interaction: discord.Interaction, button: discord.ui.Button):
         user_id = self.extract_user_id(interaction.message.content)
-        if user_id not in bot.application_data:
-            await interaction.response.send_message('Error: user_id not found in applications list!', ephemeral=True)
-        else:
-            application_data = bot.application_data.get(user_id)
-            user = interaction.guild.get_member(user_id)
+        bot.db_cursor.execute("SELECT * FROM application_data WHERE user_id=?", (user_id,))
+        application_data = bot.db_cursor.fetchone()
 
-            if user:
-                if application_data['fc'] == 'Seventh Haven':
-                    role = interaction.guild.get_role(bot.config["seventh_haven_role_id"])
-                elif application_data['fc'] == 'Moon':
-                    role = interaction.guild.get_role(bot.config["moon_role_id"])
-                bot.application_data.pop(user_id)
-                await user.add_roles(role)
-                await user.edit(nick=application_data['ingame_name'])
-                await interaction.message.delete()
-                await interaction.response.send_message(f'Application for {user.mention} approved!', ephemeral=True)
-                await user.send('Your application to get access to Seventh Haven server has been approved, you now have access to the server.')
-            else:
-                await interaction.response.send_message('Error: Failed to fetch user', ephemeral=True)
+        if not application_data:
+            await interaction.response.send_message('Error: user_id not found in applications list!', ephemeral=True)
+            return
+
+        user = interaction.guild.get_member(user_id)
+
+        if user:
+            if application_data['fc'] == 'Seventh Haven':
+                role = interaction.guild.get_role(bot.config["seventh_haven_role_id"])
+            elif application_data['fc'] == 'Moon':
+                role = interaction.guild.get_role(bot.config["moon_role_id"])
+
+            bot.db_cursor.execute("DELETE FROM application_data WHERE user_id=?", (user_id,))
+            bot.db_conn.commit()
+
+            await user.add_roles(role)
+            await user.edit(nick=application_data['ingame_name'])
+            await interaction.message.delete()
+            await interaction.response.send_message(f'Application for {user.mention} approved!', ephemeral=True)
+            await user.send('Your application to get access to Seventh Haven server has been approved, you now have access to the server.')
+        else:
+            await interaction.response.send_message('Error: Failed to fetch user', ephemeral=True)
 
     @discord.ui.button(label='Decline', style=discord.ButtonStyle.red)
     async def decline_button(self, interaction: discord.Interaction, button: discord.ui.Button):
         user_id = self.extract_user_id(interaction.message.content)
+        bot.db_cursor.execute("SELECT * FROM application_data WHERE user_id=?", (user_id,))
+        application_data = bot.db_cursor.fetchone()
+
+        if not application_data:
+            await interaction.response.send_message('Error: user_id not found in applications list!', ephemeral=True)
+            return
+
         user = bot.get_user(user_id)
 
         if user:
-            bot.application_data.pop(user_id)
+            bot.db_cursor.execute("DELETE FROM application_data WHERE user_id=?", (user_id,))
+            bot.db_conn.commit()
+
             await interaction.response.send_message(f'Application for {user.mention} declined!', ephemeral=True)
             await user.send('Your application to get access to Seventh Haven server has been declined, please try again.')
             await interaction.message.delete()
         else:
             await interaction.response.send_message('Error: Failed to fetch user', ephemeral=True)
 
+# Send application view
 class ApplicationMessage(discord.ui.View):
     @discord.ui.button(label='Seventh Haven', style=discord.ButtonStyle.blurple)
     async def seventh_haven_button(self, interaction: discord.Interaction, button: discord.ui.Button):
-        if not interaction.user.id in bot.application_data:
-            bot.application_data[interaction.user.id] = {"fc": "Seventh Haven"}
+        bot.db_cursor.execute("SELECT * FROM application_data WHERE user_id=?", (interaction.user.id,))
+        existing_data = bot.db_cursor.fetchone()
+
+        if not existing_data:
+            bot.db_cursor.execute("INSERT INTO application_data (user_id, fc) VALUES (?, ?)", (interaction.user.id, "Seventh Haven"))
+            bot.db_conn.commit()
             await interaction.response.send_modal(ApplicationModal())
         else:
             await interaction.response.send_message('You already sent an application, please wait until an administrator reviews it.', ephemeral=True)
 
     @discord.ui.button(label='Moon', style=discord.ButtonStyle.green)
     async def moon_button(self, interaction: discord.Interaction, button: discord.ui.Button):
-        if not interaction.user.id in bot.application_data:
-            bot.application_data[interaction.user.id] = {"fc": "Moon"}
+        bot.db_cursor.execute("SELECT * FROM application_data WHERE user_id=?", (interaction.user.id,))
+        existing_data = bot.db_cursor.fetchone()
+
+        if not existing_data:
+            bot.db_cursor.execute("INSERT INTO application_data (user_id, fc) VALUES (?, ?)", (interaction.user.id, "Moon"))
+            bot.db_conn.commit()
             await interaction.response.send_modal(ApplicationModal())
         else:
             await interaction.response.send_message('You already sent an application, please wait until an administrator reviews it.', ephemeral=True)
@@ -113,9 +162,27 @@ def load_config():
 
 bot = BotClient(load_config())
 
+# Add application form to current channel | !application_form
 @bot.command()
-async def application_form_message(interaction: discord.Interaction):
+async def application_form(interaction: discord.Interaction):
     await interaction.channel.send('Which FC are you member of?', view=ApplicationMessage())
+
+# Clear all applications | !application_clear
+@bot.command()
+async def application_clear(interaction: discord.Interaction):
+    bot.db_cursor.execute("DELETE FROM application_data")
+    bot.db_conn.commit()
+    interaction.response.send_message('All applications cleared.')
+
+# Delete a specific application | !application_delete <mention>
+@bot.command()
+async def application_delete(interaction: discord.Interaction, user: discord.User):
+    if not isinstance(user, discord.User):
+        interaction.response.send_message('Error: invalid user argument, format: !application_delete <mention>')
+        return
+    bot.db_cursor.execute("DELETE FROM application_data WHERE user_id=?", (user.id))
+    bot.db_conn.commit()
+    interaction.response.send_message(f'Application deleted for user {user.mention}.')
 
 # Run the bot
 bot.run(bot.config["token"])
