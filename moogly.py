bed2673884589bc7c7b9dfbada18a337300412b4
senolem <37041587+senolem@users.noreply.github.com>
2024-05-29 23:15:06 +0200

    moved maps cog to a simple task

--- moogly.py
+++ moogly.py
@@ -1,10 +1,10 @@
 import discord
-from discord.ext import commands
+from discord.ext import commands, tasks
 import json
 import sqlite3
 import traceback
 import os
-from datetime import datetime, timezone
+from datetime import datetime, timezone, timedelta
 
 class BotClient(commands.Bot):
     def __init__(self, config, dyes_fr):
@@ -36,9 +36,6 @@ class BotClient(commands.Bot):
         return await super().setup_hook()
 
     async def on_ready(self):
-        from mapsCog import MapsCog
-        await bot.add_cog(MapsCog(bot))
-
         print(f'Logged in as {self.user} (ID: {self.user.id})')
         print('------')
 
@@ -392,6 +389,50 @@ async def maps_list(interaction: discord.Interaction, message_id: int):
     )
 
     await interaction.channel.send(embed=embed)
+
+@tasks.loop(minutes=1.0)
+async def ping_task(self):
+    # Fetch maps runs that have not been pinged yet
+    bot.db_cursor.execute('SELECT * FROM maps_runs WHERE pinged=0')
+    maps_runs = bot.db_cursor.fetchall()
+
+    if not maps_runs:
+        return
+
+    current_time = datetime.now(timezone.utc)
+        
+    for maps_run in maps_runs:
+        # Extract timestamp and calculate ping time
+        timestamp = datetime.strptime(maps_run[1], '%Y-%m-%d %H:%M:%S').replace(tzinfo=timezone.utc)
+        ping_time = timestamp - timedelta(minutes=20)
+
+        if current_time >= ping_time:
+            # Fetch the joined users
+            joined_user_ids = maps_run[3].split(',')
+            joined_users = [f"<@{user_id}>" for user_id in joined_user_ids if user_id]
+
+            # Create an embed with the ping message
+            embed = discord.Embed(
+                title="Maps Run Reminder",
+                description=f"The maps run will start in 20 minutes. Are you ready?\nJoined Users: {' '.join(joined_users)}",
+                color=0xff0000
+            )
+
+            # Find the message to ping
+            message_id = maps_run[0]
+            channel_id = bot.config['events_channel_id']
+            channel = bot.get_channel(channel_id)
+            if channel:
+                try:
+                    message = await channel.fetch_message(message_id)
+                    await message.channel.send(embed=embed)
+
+                    # Update the pinged status to true
+                    bot.db_cursor.execute('UPDATE maps_runs SET pinged=1 WHERE message_id=?', (message_id,))
+                    bot.db_conn.commit()
+
+                except discord.NotFound:
+                    pass
 
 # Run the bot
 bot.run(bot.config['token'])
