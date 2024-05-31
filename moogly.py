30d670670db1c768598a727470aaaf97b5d98b11
senolem <37041587+senolem@users.noreply.github.com>
2024-05-31 18:59:43 +0200

    another way...

--- moogly.py
+++ moogly.py
@@ -21,6 +21,7 @@ class BotClient(commands.Bot):
         self.config = config
 
         self.dyes_fr = dyes_fr
+        self.ping_task.start()
 
         super().__init__(
             command_prefix=commands.when_mentioned_or(config['prefix']),
@@ -30,6 +31,7 @@ class BotClient(commands.Bot):
 
     @tasks.loop(minutes=1.0)
     async def ping_task(self):
+        await self.wait_until_ready()
         print('Checking for maps to ping...')
         # Fetch maps runs that have not been pinged yet
         self.db_cursor.execute('SELECT * FROM maps_runs WHERE pinged=0')
@@ -97,7 +99,6 @@ class BotClient(commands.Bot):
         return await super().setup_hook()
 
     async def on_ready(self):
-        await self.ping_task.start()
         print(f"Logged in as {self.user} (ID: {self.user.id})")
         print('------')
 
